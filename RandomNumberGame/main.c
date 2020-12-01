65ad1f2423215f286657a5a4850359e7d1c278b5
Lucas Damascena <75103323+ldamascena-dev@users.noreply.github.com>
2020-11-30 21:37:03 -0300

    Initial Commit - Alura - C Introduction

---
+++ RandomNumberGame/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#define ATTEMPT_NUMBER 3
+
+int main() {
+  printf("************************************* \n");
+  printf("* Welcome to the random number game * \n");
+  printf("************************************* \n");
+ 
+  int secretNumber = 42;
+  int userAttempt;
+
+  for (int i = 1; i <= ATTEMPT_NUMBER; i++) {
+    printf("Attempt %d of %d\n", i, ATTEMPT_NUMBER);  
+    printf("What's the number? ");
+    scanf("%d", & userAttempt);
+
+    int IsCorrect = (userAttempt == secretNumber);
+    int isBiggerNumber = (userAttempt > secretNumber);
+    int isLessNumber = (userAttempt < secretNumber);
+
+    if (IsCorrect) {
+      printf("Congratulations, you got it right!\n");
+      break;
+    } else if (isBiggerNumber) {
+        printf("The number entered is greater than the secret number.\n");
+    } else {
+        printf("The number entered is less than the secret number.\n");
+    }
+  }
+    
+  printf("End!\n");
+
+  return 0;
+}
