3120ab235d5fc52529a5f3cf3cbe52ab83a6d2f7
Lucas Damascena <75103323+ldamascena-dev@users.noreply.github.com>
2020-12-06 15:57:13 -0300

    Last basic lesson
    This exercise does not contain all the typing validations.

--- RandomNumberGame/main.c
+++ RandomNumberGame/main.c
@@ -7,45 +7,68 @@ int main() {
   printf("************************************* \n");
   printf("* Welcome to the random number game * \n");
   printf("************************************* \n");
- 
-  int i = 0;
+
+  short i, isBiggerNumber, isCorrect, level, numberAttempt, userAttempt = 0;
   double lostScore = 0;
   double score = 1000;
   srand(time(0));
   int secretNumber = rand() % 100;
-  int userAttempt = 0;
-  
-  while (1) {
-    
-    i++;
-    
-    printf("Attempt %d\n", i);  
-    printf("What's the number? ");
+
+  printf("Select the level \n");
+  printf("(1) - Easy\n");
+  printf("(2) - Intermediate\n");
+  printf("(3) - Hard\n");
+  scanf("%d", &level);
+
+  switch (level){
+
+    case 1:
+      numberAttempt = 20;
+      break;
+
+    case 2:
+      numberAttempt = 10;
+      break;
+
+    default:
+      numberAttempt = 5;
+      break;
+  }
+
+  for (int i = 1; i <= numberAttempt; i++) {
+
+    printf("Attempt %d\n", i);
+    printf("What's the number?\n ");
     scanf("%d", &userAttempt);
 
-    if(userAttempt < 0){
+    if (userAttempt < 0) {
       printf("Do not use negative numbers!\n");
+      i--;
       continue;
     }
 
-    int IsCorrect = (userAttempt == secretNumber);
-    int isBiggerNumber = (userAttempt > secretNumber);
+    isCorrect = (userAttempt == secretNumber);
+    isBiggerNumber = (userAttempt > secretNumber);
 
-    if (IsCorrect) {
-      printf("Congratulations, you got it right!\n");
+    if (isCorrect) {
       break;
     } else if (isBiggerNumber) {
-        printf("The number entered is greater than the secret number.\n");
+      printf("The number entered is greater than the secret number.\n");
     } else {
-        printf("The number entered is less than the secret number.\n");
+      printf("The number entered is less than the secret number.\n");
     }
 
     lostScore = (userAttempt - secretNumber) / (double) 2;
     score -= fabs(lostScore);
   }
-    
-  printf("End! Total: %d attempt(s).\n", i);
-  printf("Total Score: %.1f\n", score);
+
+  if (isCorrect) {
+    printf("Congratulations, you got it right!\n");
+    printf("End! Total: %d attempt(s).\n", i);
+    printf("Total Score: %.1f\n", score);
+  } else {
+    printf("You lost! Try again.\n");
+  }
 
   return 0;
 }
